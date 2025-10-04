a1edafdfd6e562600d03f773606d10604b8cf152
unknown <rune.struyven@student.ucll.be>
2025-10-04 13:37:50 +0200

    small change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={GlobalStyles.container}>
-      <Text style={GlobalStyles.title}>Mobile Application</Text>
+      <Text style={GlobalStyles.title}>Mobile Applications</Text>
       <Text style={GlobalStyles.subtitle}>The App</Text>
     </View>
   );
